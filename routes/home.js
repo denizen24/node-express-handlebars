64e92814fe17c427e52eda1ceaaf42c688d265b3
Vladimirov Denis <akakiy813@yandex.ru>
2019-09-25 15:34:16 +0300

    use handlebars+node+express

---
+++ routes/home.js
@@ -0,0 +1,17 @@
+const {Router} = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Главная страница',
+        isHome: true
+    })
+})
+
+
+
+
+
+
+
+module.exports = router
